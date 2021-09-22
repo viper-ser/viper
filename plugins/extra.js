3191590a5b37331914532bdb9a80039de528f4d5
Shahid-Shazz <89721494+shahid-devil@users.noreply.github.com>
2021-09-22 14:11:14 +0530

    Update extra.js

--- plugins/extra.js
+++ plugins/extra.js
@@ -128,13 +128,13 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    MyPnky.addCommand({pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
+    MyPnky.addCommand({pattern: 'luttapi ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*mwolus* 💗' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*luttapi* 🙈' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
@@ -148,7 +148,7 @@ if (config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'ᴘɪɴᴋʏ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀꜰɴᴀɴᴘʟᴋ' , MessageType.text, { quoted: message.data });
+            await message.client.sendMessage(message.jid,'ʟᴜᴛᴛᴀᴘɪ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱʜᴀʜɪᴅ' , MessageType.text, { quoted: message.data });
         }
         else {
             await message.client.sendMessage(message.jid,config.PLK, MessageType.text, { quoted: message.data });
@@ -290,13 +290,13 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    MyPnky.addCommand({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+    MyPnky.addCommand({pattern: 'luttapi ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*Mwolus* 💗' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*luttapi* 🙈' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
@@ -310,7 +310,7 @@ else if (config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'ᴘɪɴᴋʏ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀꜰɴᴀɴᴘʟᴋ' , MessageType.text, { quoted: message.data });
+            await message.client.sendMessage(message.jid,'ʟᴜᴛᴛᴀᴘɪ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱʜᴀʜɪᴅ' , MessageType.text, { quoted: message.data });
         }
         else {
             await message.client.sendMessage(message.jid,config.PLK, MessageType.text, { quoted: message.data });
