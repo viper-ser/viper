7db2261c7b517c379ea6dd1a63658bc794427cac
Shahid-Shazz <89721494+shahid-devil@users.noreply.github.com>
2021-10-14 12:46:16 +0530

    Create fakeremove.js

---
+++ plugins/fakeremove.js
@@ -0,0 +1,103 @@
+const QueenSew = require('../events');
+const {
+    MessageType
+} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const Config = require('../config')
+const RavinduManoj = 'fake number not allowed'
+
+
+
+var _0xda77 = ["jid", "user", "client", "groupMetadata", "@", "split", "isAdmin", "map", "participants", "includes", "text", "FAKE_BAN", "true", "/", "COUNTRY_CODE", ";", "94", "91", "startsWith", "participant", "data", "a", "e", "i", "o", "u", "message", "test", "sendMessage", "groupRemove", "tosophia"];
+async function checkImAdmin(_0x4edex2, _0x4edex3 = message[_0xda77[2]][_0xda77[1]][_0xda77[0]]) {
+    var _0x4edex4 = await _0x4edex2[_0xda77[2]][_0xda77[3]](_0x4edex2[_0xda77[0]]);
+    var _0x4edex5 = _0x4edex4[_0xda77[8]][_0xda77[7]]((_0x4edex6) => {
+        if (_0x4edex6[_0xda77[0]][_0xda77[5]](_0xda77[4])[0] == _0x4edex3[_0xda77[5]](_0xda77[4])[0] && _0x4edex6[_0xda77[6]]) {
+            return true
+        } else {
+            ;
+        };
+        return false
+    });
+    return _0x4edex5[_0xda77[9]](true)
+}
+QueenSew[_0xda77[30]]({
+    on: _0xda77[10],
+    fromMe: false,
+    onlyGroup: true
+}, (async (_0x4edex2, _0x4edex7) => {
+    if (Config[_0xda77[11]] = _0xda77[12]) {
+        var _0x4edex8, _0x4edex9;
+        if (Config[_0xda77[14]][_0xda77[9]](_0xda77[13])) {
+            var _0x4edexa = Config[_0xda77[14]][_0xda77[5]](_0xda77[15]);
+            _0x4edex8 = _0x4edexa[1];
+            _0x4edex9 = _0x4edexa[0]
+        } else {
+            _0x4edex8 = Config[_0xda77[14]];
+            _0x4edex9 = _0xda77[16]
+        };
+        if (_0x4edex2[_0xda77[20]][_0xda77[19]][_0xda77[18]](_0xda77[17]) || _0x4edex2[_0xda77[20]][_0xda77[19]][_0xda77[18]](_0xda77[16]) || _0x4edex2[_0xda77[20]][_0xda77[19]][_0xda77[18]](_0x4edex8) || _0x4edex2[_0xda77[20]][_0xda77[19]][_0xda77[18]](_0x4edex9)) {
+            return
+        } else {
+            let _0x4edexb = new RegExp(_0xda77[21]);
+            let _0x4edexc = new RegExp(_0xda77[22]);
+            let _0x4edexd = new RegExp(_0xda77[23]);
+            let _0x4edexe = new RegExp(_0xda77[24]);
+            let _0x4edexf = new RegExp(_0xda77[25]);
+            if (_0x4edexb[_0xda77[27]](_0x4edex2[_0xda77[26]])) {
+                var _0x4edex10 = await checkImAdmin(_0x4edex2);
+                if (!_0x4edex10) {
+                    return
+                };
+                await _0x4edex2[_0xda77[2]][_0xda77[28]](_0x4edex2[_0xda77[0]], RavinduManoj, MessageType[_0xda77[10]], {
+                    quoted: _0x4edex2[_0xda77[20]]
+                });
+                await _0x4edex2[_0xda77[2]][_0xda77[29]](_0x4edex2[_0xda77[0]], [_0x4edex2[_0xda77[20]][_0xda77[19]]])
+            } else {
+                if (_0x4edexc[_0xda77[27]](_0x4edex2[_0xda77[26]])) {
+                    var _0x4edex10 = await checkImAdmin(_0x4edex2);
+                    if (!_0x4edex10) {
+                        return
+                    };
+                    await _0x4edex2[_0xda77[2]][_0xda77[28]](_0x4edex2[_0xda77[0]], RavinduManoj, MessageType[_0xda77[10]], {
+                        quoted: _0x4edex2[_0xda77[20]]
+                    });
+                    await _0x4edex2[_0xda77[2]][_0xda77[29]](_0x4edex2[_0xda77[0]], [_0x4edex2[_0xda77[20]][_0xda77[19]]])
+                } else {
+                    if (_0x4edexd[_0xda77[27]](_0x4edex2[_0xda77[26]])) {
+                        var _0x4edex10 = await checkImAdmin(_0x4edex2);
+                        if (!_0x4edex10) {
+                            return
+                        };
+                        await _0x4edex2[_0xda77[2]][_0xda77[28]](_0x4edex2[_0xda77[0]], RavinduManoj, MessageType[_0xda77[10]], {
+                            quoted: _0x4edex2[_0xda77[20]]
+                        });
+                        await _0x4edex2[_0xda77[2]][_0xda77[29]](_0x4edex2[_0xda77[0]], [_0x4edex2[_0xda77[20]][_0xda77[19]]])
+                    } else {
+                        if (_0x4edexe[_0xda77[27]](_0x4edex2[_0xda77[26]])) {
+                            var _0x4edex10 = await checkImAdmin(_0x4edex2);
+                            if (!_0x4edex10) {
+                                return
+                            };
+                            await _0x4edex2[_0xda77[2]][_0xda77[28]](_0x4edex2[_0xda77[0]], RavinduManoj, MessageType[_0xda77[10]], {
+                                quoted: _0x4edex2[_0xda77[20]]
+                            });
+                            await _0x4edex2[_0xda77[2]][_0xda77[29]](_0x4edex2[_0xda77[0]], [_0x4edex2[_0xda77[20]][_0xda77[19]]])
+                        } else {
+                            if (_0x4edexf[_0xda77[27]](_0x4edex2[_0xda77[26]])) {
+                                var _0x4edex10 = await checkImAdmin(_0x4edex2);
+                                if (!_0x4edex10) {
+                                    return
+                                };
+                                await _0x4edex2[_0xda77[2]][_0xda77[28]](_0x4edex2[_0xda77[0]], RavinduManoj, MessageType[_0xda77[10]], {
+                                    quoted: _0x4edex2[_0xda77[20]]
+                                });
+                                await _0x4edex2[_0xda77[2]][_0xda77[29]](_0x4edex2[_0xda77[0]], [_0x4edex2[_0xda77[20]][_0xda77[19]]])
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}))
